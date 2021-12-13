e6dd96cd16b605585df4b6a309ccb15e3f302124
Allan Tokuda <allan.tokuda@gmail.com>
2021-12-12 22:10:34 -0600

    2021 day 12 with Neo4j path finding

---
+++ 2021/12/convert_to_cipher.rb
@@ -0,0 +1,24 @@
+paths = File.readlines(ARGV[0], chomp: true).map { |line| line.split('-') }
+
+class String
+  def is_big?
+    self[0] == self[0].upcase
+  end
+end
+
+puts 'CREATE' + [
+  paths.flatten.uniq.map do |node|
+    labels = if ['start', 'end'].include?(node)
+             node.capitalize
+           elsif node.is_big?
+             'BigCave'
+           else
+             'SmallCave'
+           end
+    "(#{node}:#{labels} {name: '#{node}'})"
+  end.to_a +
+  paths.map do |path|
+    path.reverse! if path.last == 'start' || path.first == 'end'
+    path.map { |node| "(#{node})" }.join('-[:path]->')
+  end.to_a
+].join(",\n") + ';'
