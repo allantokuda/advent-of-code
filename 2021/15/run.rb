1028ced0e96f51b72096f15a548da668b32ac260
Allan Tokuda <allan.tokuda@gmail.com>
2021-12-25 16:59:53 -0600

    Rewrite with Dijkstra's algorithm

--- 2021/15/run.rb
+++ 2021/15/run.rb
@@ -1,36 +1,67 @@
 grid = File.read(ARGV[0]).split("\n").map { |line| line.chars.map(&:to_i) }
 
-xmax = grid.length - 1
-ymax = grid.first.length - 1
 
-# "Multiple best" finder, also allowing multiple endpoints: scored based on Manhattan progress
 
-def best_paths(grid, x1, y1, x2, y2, target_manhattan, limit=100, x=x1, y=y1, risk = 0, path = [], start_point = true, backtracks = 0)
-  return [] if backtracks > 2 || !(x1..x2).include?(x) || !(y1..y2).include?(y) || (!start_point && path.include?([x, y]))
-  new_path = path
-  new_risk = risk
-  unless start_point
-    new_path += [[x, y]]
-    new_risk += grid[x][y]
-    #puts backtracks.to_s + ' -> ' + path.inspect
+
+
+class Node
+  attr_accessor :row_num, :col_num, :path, :node_risk, :total_risk, :closed
+
+  def initialize(row_num, col_num, node_risk)
+    @node_risk = node_risk
+    @closed = false
+    @row_num = row_num
+    @col_num = col_num
   end
-  manhattan_progress = x + y
-  return [{ risk: new_risk, manhattan: manhattan_progress, path: new_path }] if manhattan_progress == target_manhattan
-  results = []
-  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x+1, y, new_risk, new_path, false, backtracks)
-  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x, y+1, new_risk, new_path, false, backtracks)
-  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x-1, y, new_risk, new_path, false, backtracks + 1)
-  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x, y-1, new_risk, new_path, false, backtracks + 1)
-  results.sort_by { |r| r[:risk] }.group_by { |p| p[:path].last }.map { |k, v| v.first }.first(limit)
+
+  def close!
+    closed = true
+  end
+
+  def adjacent_nodes(grid)
+    [
+      (grid[row_num+1][col_num] if row_num < grid.count - 1), # down
+      (grid[row_num][col_num+1] if col_num < grid.first.count - 1), # right
+      (grid[row_num-1][col_num] if row_num > 0), # up
+      (grid[row_num][col_num-1] if col_num > 0), # left
+    ].compact.reject(&:total_risk)
+  end
+
+  def set_source(source_node)
+    # puts "set_source"
+    # puts self
+    # puts source_node.path.inspect
+    # puts source_node.total_risk.inspect
+    # puts
+    @path = source_node.path + [self]
+    @total_risk = source_node.total_risk + node_risk
+  end
+
+  def coords
+    [row_num,col_num]
+  end
+
+  def coords_str
+    coords.join(',')
+  end
 end
 
-results = best_paths(grid, 0,0,xmax,ymax, 13)
-
-until results.detect { |r| r[:manhattan] >= xmax + ymax  }
-  results = results.map do |result|
-    best_paths(grid, 0, 0, xmax, ymax, [result[:manhattan] + 3, xmax + ymax].min, 100, *result[:path][-1], result[:risk], result[:path])
-  end.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(200)
-  puts results.first.slice(:manhattan, :risk)
+node_grid = grid.each_with_index.map { |row, i| row.each_with_index.map { |risk, j| Node.new(i, j, risk) } }
+start_node = node_grid[0][0]
+end_node = node_grid[-1][-1]
+start_node.path = []
+start_node.total_risk = 0
+open_nodes = [start_node]
+closed_nodes = []
+while(end_node.total_risk.nil?)
+  n = open_nodes.sort_by(&:total_risk).first # nearest open node
+  n.close!
+  adj = n.adjacent_nodes(node_grid)
+  adj.map { |a| a.set_source(n) }
+  closed_nodes += [n]
+  open_nodes -= [n]
+  open_nodes += adj
+  #puts "#{closed_nodes.count}/#{10000}: #{open_nodes.map(&:coords_str).join('  ')}" # DEBUG
 end
 
 def paint_path(grid, path, color_code=31)
@@ -40,26 +71,8 @@ def paint_path(grid, path, color_code=31)
 end
 
 g = grid.map(&:dup)
-results.each_with_index do |r, i|
-  paint_path(g, r, 31 + i % 6)
-end
+path = end_node.path.map(&:coords)
+paint_path(g, path)
 puts g.map { |row| row.join }
 
-puts
-puts "Risk: #{results.first[:risk]}"
-
-results = (0..xmax).map { |x| y = xmax - x; best_paths(grid, 0, 0, xmax, ymax, x+y+3, x, y) }.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(10)
-
-results = []
-(0..19).each do |diagonal|
-  manhattan = diagonal * 10
-  puts [manhattan, results.count].inspect
-  x_range, y_func = begin
-    if manhattan <= xmax
-      [(0..manhattan), ->(x) { ymax - x - manhattan }]
-    else
-      [((manhattan - xmax)..xmax), ->(x) { ymax - x }]
-    end
-  end
-  results += x_range.map { |x| y = y_func.call(x); best_paths(grid, 0, 0, xmax, ymax, x+y+10, x, y) }.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(10)
-end
+puts "Total risk: #{end_node.total_risk}"
