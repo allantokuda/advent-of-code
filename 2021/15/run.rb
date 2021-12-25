f153464333dd3822b869d46d94eac72de81627e6
Allan Tokuda <allan.tokuda@gmail.com>
2021-12-25 17:55:36 -0600

    Part 2 with 25x larger search space

--- 2021/15/run.rb
+++ 2021/15/run.rb
@@ -1,9 +1,5 @@
 grid = File.read(ARGV[0]).split("\n").map { |line| line.chars.map(&:to_i) }
 
-
-
-
-
 class Node
   attr_accessor :row_num, :col_num, :path, :node_risk, :total_risk
 
@@ -23,11 +19,6 @@ class Node
   end
 
   def set_source(source_node)
-    # puts "set_source"
-    # puts self
-    # puts source_node.path.inspect
-    # puts source_node.total_risk.inspect
-    # puts
     @path = source_node.path + [self]
     @total_risk = source_node.total_risk + node_risk
   end
@@ -41,32 +32,54 @@ class Node
   end
 end
 
-node_grid = grid.each_with_index.map { |row, i| row.each_with_index.map { |risk, j| Node.new(i, j, risk) } }
-start_node = node_grid[0][0]
-end_node = node_grid[-1][-1]
-start_node.path = []
-start_node.total_risk = 0
-open_nodes = [start_node]
-closed_nodes = []
-while(end_node.total_risk.nil?)
-  n = open_nodes.sort_by(&:total_risk).first # nearest open node
-  adj = n.adjacent_nodes(node_grid)
-  adj.map { |a| a.set_source(n) }
-  closed_nodes += [n]
-  open_nodes -= [n]
-  open_nodes += adj
-  #puts "#{closed_nodes.count}/#{10000}: #{open_nodes.map(&:coords_str).join('  ')}" # DEBUG
+def node_grid_from_values(value_grid)
+  value_grid.each_with_index.map { |row, i| row.each_with_index.map { |risk, j| Node.new(i, j, risk) } }
+end
+
+def dijkstra_search(value_grid)
+  node_grid = node_grid_from_values(value_grid)
+  start_node = node_grid[0][0]
+  end_node = node_grid[-1][-1]
+  start_node.path = []
+  start_node.total_risk = 0
+  open_nodes = [start_node]
+  closed_nodes = []
+  while(end_node.total_risk.nil?)
+    n = open_nodes.sort_by(&:total_risk).first # nearest open node
+    adj = n.adjacent_nodes(node_grid)
+    adj.map { |a| a.set_source(n) }
+    closed_nodes += [n]
+    open_nodes -= [n]
+    open_nodes += adj
+  end
+  end_node
 end
 
 def paint_path(grid, path, color_code=31)
+  g = grid.map(&:dup)
   path.each do |x,y|
-    grid[x][y] = "\e[#{color_code}m#{grid[x][y]}\e[0m"
+    g[x][y] = "\e[#{color_code}m#{g[x][y]}\e[0m"
   end
+  puts g.map { |row| row.join }
 end
 
-g = grid.map(&:dup)
-path = end_node.path.map(&:coords)
-paint_path(g, path)
-puts g.map { |row| row.join }
+end_node1 = dijkstra_search(grid)
+paint_path(grid, end_node1.path.map(&:coords))
+puts "Part 1: total risk: #{end_node1.total_risk}"
 
-puts "Total risk: #{end_node.total_risk}"
+# Part 2: 25x larger map!
+grid2 = begin
+  (0..4).map do |i|
+    grid.map(&:dup).map do |row|
+      (0..4).map do |j|
+        row.map do |cell|
+          (cell + i + j - 1) % 9 + 1
+        end
+      end.flatten(1)
+    end
+  end.flatten(1)
+end
+
+end_node2 = dijkstra_search(grid2)
+paint_path(grid2, end_node2.path.map(&:coords))
+puts "Part 2: total risk: #{end_node2.total_risk}"
