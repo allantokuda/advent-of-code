67d5da30f5047609fcf9d951e3a3a0d8f3984593
Allan Tokuda <allan.tokuda@gmail.com>
2021-12-25 16:52:35 -0600

    More experimentation, getting closer

--- 2021/15/run.rb
+++ 2021/15/run.rb
@@ -4,31 +4,37 @@ xmax = grid.length - 1
 ymax = grid.first.length - 1
 
 # "Multiple best" finder, also allowing multiple endpoints: scored based on Manhattan progress
-def best_paths(grid, x1, y1, x2, y2, target_manhattan, risk = 0, path = [], start_point: true)
+
+def best_paths(grid, x1, y1, x2, y2, target_manhattan, limit=100, x=x1, y=y1, risk = 0, path = [], start_point = true, backtracks = 0)
+  return [] if backtracks > 2 || !(x1..x2).include?(x) || !(y1..y2).include?(y) || (!start_point && path.include?([x, y]))
   new_path = path
   new_risk = risk
   unless start_point
-    new_path += [[x1, y1]]
-    new_risk += grid[x1][y1]
+    new_path += [[x, y]]
+    new_risk += grid[x][y]
+    #puts backtracks.to_s + ' -> ' + path.inspect
   end
-  manhattan_progress = x1 + y1
+  manhattan_progress = x + y
   return [{ risk: new_risk, manhattan: manhattan_progress, path: new_path }] if manhattan_progress == target_manhattan
   results = []
-  results += best_paths(grid, x1 + 1, y1, x2, y2, target_manhattan, new_risk, new_path, start_point: false) if x2 > x1
-  results += best_paths(grid, x1, y1 + 1, x2, y2, target_manhattan, new_risk, new_path, start_point: false) if y2 > y1
-  results.sort_by { |r| r[:risk] }.group_by { |p| p[:path].last }.map { |k, v| v.first }.first(20)
+  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x+1, y, new_risk, new_path, false, backtracks)
+  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x, y+1, new_risk, new_path, false, backtracks)
+  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x-1, y, new_risk, new_path, false, backtracks + 1)
+  results += best_paths(grid, x1, y1, x2, y2, target_manhattan, limit, x, y-1, new_risk, new_path, false, backtracks + 1)
+  results.sort_by { |r| r[:risk] }.group_by { |p| p[:path].last }.map { |k, v| v.first }.first(limit)
 end
 
-results = best_paths(grid, 0,0,xmax,ymax, 16)
+results = best_paths(grid, 0,0,xmax,ymax, 13)
 
 until results.detect { |r| r[:manhattan] >= xmax + ymax  }
-  results = results.map do |path|
-    best_paths(grid, *path[:path][-1], xmax, ymax, [path[:manhattan] + 4, xmax + ymax].min, path[:risk], path[:path])
-  end.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(100)
+  results = results.map do |result|
+    best_paths(grid, 0, 0, xmax, ymax, [result[:manhattan] + 3, xmax + ymax].min, 100, *result[:path][-1], result[:risk], result[:path])
+  end.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(200)
+  puts results.first.slice(:manhattan, :risk)
 end
 
-def paint_path(grid, result, color_code=31)
-  result[:path].reverse.each do |x,y|
+def paint_path(grid, path, color_code=31)
+  path.each do |x,y|
     grid[x][y] = "\e[#{color_code}m#{grid[x][y]}\e[0m"
   end
 end
@@ -41,3 +47,19 @@ puts g.map { |row| row.join }
 
 puts
 puts "Risk: #{results.first[:risk]}"
+
+results = (0..xmax).map { |x| y = xmax - x; best_paths(grid, 0, 0, xmax, ymax, x+y+3, x, y) }.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(10)
+
+results = []
+(0..19).each do |diagonal|
+  manhattan = diagonal * 10
+  puts [manhattan, results.count].inspect
+  x_range, y_func = begin
+    if manhattan <= xmax
+      [(0..manhattan), ->(x) { ymax - x - manhattan }]
+    else
+      [((manhattan - xmax)..xmax), ->(x) { ymax - x }]
+    end
+  end
+  results += x_range.map { |x| y = y_func.call(x); best_paths(grid, 0, 0, xmax, ymax, x+y+10, x, y) }.flatten.sort_by { |r| r[:risk] }.group_by { |r| r[:path].last }.map { |k, v| v.first }.first(10)
+end
