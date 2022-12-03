f46380772722475c6b7a5ad4fe98a81bc1051eae
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-02 22:35:52 -0600

    Shorter with &:split

--- 2022/02/run.rb
+++ 2022/02/run.rb
@@ -1,4 +1,4 @@
-lines = File.read(ARGV[0]).split("\n").map { |line| line.split(" ") }.map { |a,b| [a.ord-64, b.ord-87] }
+lines = File.read(ARGV[0]).split("\n").map(&:split).map { |a,b| [a.ord-64, b.ord-87] }
 
 # part 1
 puts lines.map { |a,b| ((b-a)%3+1)%3*3 + b }.sum
