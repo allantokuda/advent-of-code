1775751073e9dc591245a153b0fb38e0cfad24c9
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-04 15:44:18 -0600

    Day 4 part 2 Clojure

--- 2022/04/run.clj
+++ 2022/04/run.clj
@@ -1,7 +1,7 @@
 ; Usage: clojure -M run.clj test
 
 (use '[clojure.string :only [index-of split join]])
-(use '[clojure.set :only [difference]])
+(use '[clojure.set :only [difference intersection]])
 
 ; Generic tooling for readability
 (defn split_map [delim coll fn] (map fn (split coll delim)))
@@ -17,3 +17,7 @@
 ; Part 1
 (defn full_overlap [pair] (if (empty? (apply difference pair)) 1 0))
 (println (reduce + (map full_overlap pairs)))
+
+; Part 2
+(defn any_overlap [pair] (if (empty? (apply intersection pair)) 0 1))
+(println (reduce + (map any_overlap pairs)))
