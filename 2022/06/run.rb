c9eda72d7587bb666bb71fec0788f847b2fbcb68
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-06 07:24:06 -0600

    Refactor day 6

--- 2022/06/run.rb
+++ 2022/06/run.rb
@@ -1,21 +1,14 @@
-chars = ARGF.read.chars
+str = ARGF.read
 
-last4 = ''
-chars.each_with_index do |char, i|
-  last4 += char
-  last4 = last4[1..] if last4.length == 5
-  if last4.length == 4 && last4.chars.uniq.join == last4
-    puts i + 1
-    break
+# A "marker" in a string is defined as the first index at which
+# n characters leading up to that index are all different.
+def find_marker(str, n)
+  buffer = str[0] * n
+  str.chars.each_with_index do |char, i|
+    buffer = (buffer + char)[-n..-1]
+    return i + 1 if buffer.chars.uniq.count == n
   end
 end
 
-last14 = ''
-chars.each_with_index do |char, i|
-  last14 += char
-  last14 = last14[1..] if last14.length == 15
-  if last14.length == 14 && last14.chars.uniq.join == last14
-    puts i + 1
-    break
-  end
-end
+puts find_marker(str,  4) # Part 1
+puts find_marker(str, 14) # Part 2
