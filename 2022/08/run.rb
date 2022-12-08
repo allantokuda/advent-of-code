33a5c17743c24c56bd5a1b327d9376ed169ccabe
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-08 07:30:08 -0600

    Refactor day 8 part 1

--- 2022/08/run.rb
+++ 2022/08/run.rb
@@ -1,23 +1,18 @@
 tree_rows = ARGF.read.split("\n").map { |row| row.chars.map { |char| { h: char.to_i } } }
 
-[
-  tree_rows,
-  tree_rows.transpose,
-  tree_rows.map(&:reverse),
-  tree_rows.transpose.map(&:reverse)
-].each do |transformation|
-  transformation.each do |row|
-    visible_height = -1
-    row.each do |tree|
-      if tree[:h] > visible_height
-        visible_height = tree[:h]
-        tree[:v] = true
+[tree_rows, tree_rows.transpose].each do |horiz_vert_rows|
+  [horiz_vert_rows, horiz_vert_rows.map(&:reverse)].each do |any_direction_rows|
+    any_direction_rows.each do |any_direction_row|
+      visible_height = -1
+      any_direction_row.each do |tree|
+        if tree[:h] > visible_height
+          visible_height = tree[:h]
+          tree[:v] = true
+        end
       end
     end
   end
 end
-
-puts tree_rows.inspect
 
 # Part 1
 puts tree_rows.sum { |row| row.count { |tree| tree[:v] }}
