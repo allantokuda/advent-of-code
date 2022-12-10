586c9f714e414b6a10e4437bd5c5fa91472dc8c5
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-10 17:17:46 -0600

    Cleanup and refactor

--- 2022/10/run.rb
+++ 2022/10/run.rb
@@ -5,27 +5,19 @@ DURATION = { 'addx' => 2, 'noop' => 1 }
 cooldown = 0
 i = 1
 x = 1
-x_history = []
+history = []
+
 while commands.any? || cooldown > 0
-  puts i
-  puts "start x = #{x}  cooldown: #{cooldown}"
-  x_history << { i: i, x: x }
+  history << { i: i, x: x }
   if cooldown == 0
     command = commands.shift
-    puts "  begin #{command}"
     cooldown = DURATION[command[:name]]
+  elsif cooldown == 1 && command[:name] == 'addx'
+    x += command[:value]
   end
   cooldown -= 1
-  if cooldown == 0 && command[:name] == 'addx'
-    x += command[:value]
-    puts "  now add #{command[:value]}"
-  end
-  puts "end   x = #{x}"
-  puts
   i += 1
 end
 
-puts x_history.map {|h| h.values.join(' ') }
-
 keypoints = (0..5).map { |n| 20 + n*40 }
-puts keypoints.map { |k| x_history[k-1].values.inject(:*) }.sum
+puts keypoints.map { |k| history[k-1].values.inject(:*) }.sum
