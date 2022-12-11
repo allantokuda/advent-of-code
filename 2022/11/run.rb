90093c543ded3be30435327c37438e11c2b30a77
Allan Tokuda <allan.tokuda@gmail.com>
2022-12-11 17:03:26 -0600

    Ruby day 11 part 2

--- 2022/11/run.rb
+++ 2022/11/run.rb
@@ -1,7 +1,10 @@
+# Usage:
+#   ruby run.rb input 20 3     # part 1
+#   ruby run.rb input 10000 1  # part 2
 Item = Struct.new(:value)
 
 class Monkey
-  attr_reader :id, :items, :operation, :test, :inspection_count
+  attr_reader :id, :items, :operation, :test, :inspection_count, :divisor
   def initialize(text)
     lines = text.split("\n")
     @id = lines[0].scan(/\d+/).first
@@ -15,12 +18,12 @@ class Monkey
     @inspection_count = 0
   end
 
-  def throw_all
+  def throw_all(round_divisor, grand_mod)
     thrown_items, @items = items, []
     thrown_items.map do |item|
       operand1 = item.value
       operand2 = @operand2 == 'old' ? item.value : @operand2.to_i
-      item.value = [operand1, operand2].inject(@operator.to_sym) / 3
+      item.value = ([operand1, operand2].inject(@operator.to_sym) / round_divisor) % grand_mod
       to_monkey = item.value % @divisor == 0 ? @true_monkey : @false_monkey
       @inspection_count += 1
       [to_monkey, item]
@@ -32,14 +35,18 @@ class Monkey
   end
 end
 
-monkeys = ARGF.read.split("\n\n").map { |text| Monkey.new(text) }
+monkeys = File.read(ARGV[0]).split("\n\n").map { |text| Monkey.new(text) }
+round_count = ARGV[1].to_i
+worry_divisor = ARGV[2].to_i
 
-20.times do |round|
+# Keep the number size down :)
+grand_mod = monkeys.map(&:divisor).inject(:*)
+
+round_count.times do |round|
   monkeys.each do |monkey|
-    monkey.throw_all.each do |to_monkey, item|
+    monkey.throw_all(worry_divisor, grand_mod).each do |to_monkey, item|
       monkeys[to_monkey].catch(item)
     end
   end
 end
-
 puts monkeys.map(&:inspection_count).sort.reverse.first(2).inject(:*)
